b4f035c1313410f870a2c966a293a74f0a978db3
Rodrigo Siqueira <rodrigo.dsiqueira1@gmail.com>
2025-12-02 11:20:42 -0300

    Refatora a classe FormRepositoryMock para ajustar a inicialização de formulários, alterando tipos de dados e melhorando a estrutura, além de incluir parâmetros de data nas funções de atualização, cancelamento e conclusão.

--- src/shared/infra/repositories/form_repository_mock.py
+++ src/shared/infra/repositories/form_repository_mock.py
@@ -2,8 +2,8 @@ from datetime import datetime, timedelta
 from typing import List, Optional
 from src.shared.domain.entities.field import TextField
 from src.shared.domain.entities.form import Form
-from src.shared.domain.entities.information_field import TextInformationField
-from src.shared.domain.entities.justification import Justification, JustificationOption
+from src.shared.domain.entities.information_field import ImageInformationField
+from src.shared.domain.entities.justification import Justification, JustificationOption, SelectedJustification
 from src.shared.domain.entities.section import Section
 from src.shared.domain.enums.form_status_enum import FORM_STATUS
 from src.shared.domain.enums.priority_enum import PRIORITY
@@ -23,10 +23,10 @@ class FormRepositoryMock(IFormRepository):
             timestamp_yesterday_milliseconds = timestamp_yesterday_seconds * 1000
             return timestamp_yesterday_milliseconds
 
-        text_field = TextField(placeholder='placeholder', required=True, key='key', regex='regex', formatting='formatting', max_length=10, value='value')
-        section = Section(section_id='99999', fields=[text_field, text_field])
-        information_field = TextInformationField(
-            value='value'
+        text_field = TextField(label='label', required=True, key='key', order=1, regex='regex', max_length=10, value='value')
+        section = Section(section_id=1, fields=[text_field, text_field])
+        information_field = ImageInformationField(
+            file_path='image'
         )
         justification_option = JustificationOption(
             option='option',
@@ -36,19 +36,15 @@ class FormRepositoryMock(IFormRepository):
 
         justification = Justification(
             options=[justification_option],
-            selected_option='option',
-            justification_text='text',
-            justification_image='image'
+            selected=SelectedJustification(option='option', text='text', image_url='image')
         )
 
         self.forms = [
             Form(
-                form_title='FORM_TITLE',                
-                form_id='d61dbf66-a10f-11ed-a8fc-0242ac120010',
-                creator_user_id='d61dbf66-a10f-11ed-a8fc-0242ac120001',
+                id='d61dbf66-a10f-11ed-a8fc-0242ac120010',
+                form_title='FORM_TITLE',
+                created_by='d61dbf66-a10f-11ed-a8fc-0242ac120001',
                 user_id='d61dbf66-a10f-11ed-a8fc-0242ac120001',
-                vinculation_form_id='d61dbf66-a10f-11ed-a8fc-0242ac120010',
-                can_vinculate=True,
                 template='TEMPLATE',
                 area='1',
                 system='GAIA',
@@ -57,29 +53,24 @@ class FormRepositoryMock(IFormRepository):
                 number=1,
                 latitude=1.0,
                 longitude=1.0,
-                region='REGION',
-                description=None,
-                priority=PRIORITY.EMERGENCY,
+                priority=PRIORITY.THREE,
                 status=FORM_STATUS.IN_PROGRESS,
                 expiration_date=946407600000,
-                creation_date=946407600000,
-                start_date=946407600000,
-                conclusion_date=None,
+                observation=None,
+                created_at=946407600000,
+                updated_at=946407600000,
+                in_progress_at=946407600000,
+                cancelled_at=None,
+                completed_at=None,
                 justification=justification,
-                comments=None,
                 sections=[section, section],
-                information_fields=[
-                    information_field,
-                    information_field,
-                ]
+                information_fields=[information_field, information_field],
             ),
             Form(
+                id='d61dbf66-a10f-11ed-a8fc-0242ac120011',
                 form_title='FORM_TITLE2',
-                form_id='d61dbf66-a10f-11ed-a8fc-0242ac120011',
-                creator_user_id='d61dbf66-a10f-11ed-a8fc-0242ac120001',
+                created_by='d61dbf66-a10f-11ed-a8fc-0242ac120001',
                 user_id='d61dbf66-a10f-11ed-a8fc-0242ac120002',
-                vinculation_form_id=None,
-                can_vinculate=False,
                 template='TEMPLATE',
                 area='1',
                 system='GAIA',
@@ -88,29 +79,24 @@ class FormRepositoryMock(IFormRepository):
                 number=1,
                 latitude=1.0,
                 longitude=1.0,
-                region='REGION',
-                description=None,
-                priority=PRIORITY.EMERGENCY,
-                status=FORM_STATUS.CONCLUDED,
+                priority=PRIORITY.THREE,
+                status=FORM_STATUS.COMPLETED,
                 expiration_date=946407600000,
-                creation_date=946407600000,
-                start_date=946407600000,
-                conclusion_date=946407600000,
+                observation=None,
+                created_at=946407600000,
+                updated_at=946407600000,
+                in_progress_at=946407600000,
+                cancelled_at=None,
+                completed_at=946407600000,
                 justification=justification,
-                comments=None,
                 sections=[section],
-                information_fields=[
-                    information_field,
-                    information_field,
-                ]
+                information_fields=[information_field, information_field],
             ),
             Form(
-                form_title='FORM_TITLE',                
-                form_id='d61dbf66-a10f-11ed-a8fc-0242ac120012',
-                creator_user_id='d61dbf66-a10f-11ed-a8fc-0242ac120001',
+                id='d61dbf66-a10f-11ed-a8fc-0242ac120012',
+                form_title='FORM_TITLE',
+                created_by='d61dbf66-a10f-11ed-a8fc-0242ac120001',
                 user_id='d61dbf66-a10f-11ed-a8fc-0242ac120001',
-                vinculation_form_id='d61dbf66-a10f-11ed-a8fc-0242ac120010',
-                can_vinculate=True,
                 template='TEMPLATE',
                 area='1',
                 system='GAIA',
@@ -119,27 +105,24 @@ class FormRepositoryMock(IFormRepository):
                 number=1,
                 latitude=1.0,
                 longitude=1.0,
-                region='REGION',
-                description=None,
-                priority=PRIORITY.EMERGENCY,
-                status=FORM_STATUS.NOT_STARTED,
+                priority=PRIORITY.ZERO,
+                status=FORM_STATUS.PENDING,
                 expiration_date=946407600000,
-                creation_date=946407600000,
-                start_date=None,
-                conclusion_date=None,
+                observation=None,
+                created_at=946407600000,
+                updated_at=946407600000,
+                in_progress_at=None,
+                cancelled_at=None,
+                completed_at=None,
                 justification=justification,
-                comments=None,
                 sections=[section, section],
-                information_fields=[
-                    information_field,
-                    information_field,
-                ]
+                information_fields=[information_field, information_field],
             ),
         ]
     
     def get_form_by_id(self, user_id: str, form_id: str) -> Form:
         for form in self.forms:
-            if form.form_id == form_id:
+            if form.id == form_id:
                 return form
         return None
     
@@ -153,32 +136,36 @@ class FormRepositoryMock(IFormRepository):
     
     def create_form(self, form: Form) -> Form:
         for item in self.forms:
-            if form.form_id == item.form_id:
+            if form.id == item.id:
                 raise DuplicatedItem('Formulário já existe')
         self.forms.append(form)
         return form
     
-    def update_form_status(self, user_id: str, form_id: str, status: FORM_STATUS, start_date: Optional[int] = None) -> Form:
+    def update_form_status(self, user_id: str, form_id: str, status: FORM_STATUS, in_progress_at: Optional[int] = None, updated_at: Optional[int] = None) -> Form:
         for form in self.forms:
-            if form.form_id == form_id:
+            if form.id == form_id:
                 form.status = status
-                form.start_date = start_date
+                form.in_progress_at = in_progress_at
+                form.updated_at = updated_at or form.updated_at
                 return form
         return None
     
-    def cancel_form(self, user_id: str, form_id: str, justification: Justification) -> Form:
+    def cancel_form(self, user_id: str, form_id: str, justification: Justification, cancelled_at: int, updated_at: int) -> Form:
         for form in self.forms:
-            if form.form_id == form_id:
-                form.status = FORM_STATUS.CANCELED
+            if form.id == form_id:
+                form.status = FORM_STATUS.CANCELLED
                 form.justification = justification
+                form.cancelled_at = cancelled_at
+                form.updated_at = updated_at
                 return form
         return None
 
-    def complete_form(self, user_id: str, form_id: str, sections: List[Section], vinculation_form_id: Optional[str] = None) -> Form:
+    def complete_form(self, user_id: str, form_id: str, sections: List[Section], completed_at: int, updated_at: int) -> Form:
         for form in self.forms:
-            if form.form_id == form_id:
-                form.status = FORM_STATUS.CONCLUDED
+            if form.id == form_id:
+                form.status = FORM_STATUS.COMPLETED
                 form.sections = sections
-                form.vinculation_form_id = vinculation_form_id
+                form.completed_at = completed_at
+                form.updated_at = updated_at
                 return form
         return None
