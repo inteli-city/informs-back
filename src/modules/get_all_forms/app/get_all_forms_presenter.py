14da661e56e181b9c33270045b846077ef821fba
Rodrigo Siqueira <rodrigo.dsiqueira1@gmail.com>
2025-12-04 15:08:51 -0300

    Implementa controlador e caso de uso para obter todos os formulários, incluindo tratamento de erros e mapeamento de dados para a resposta.

---
+++ src/modules/get_all_forms/app/get_all_forms_presenter.py
@@ -0,0 +1,17 @@
+from .get_all_forms_controller import GetAllFormsController
+from .get_all_forms_usecase import GetAllFormsUsecase
+from src.shared.environments import Environments
+from src.shared.helpers.external_interfaces.http_lambda_requests import LambdaHttpRequest, LambdaHttpResponse
+
+
+repo = Environments.get_form_repo()()
+usecase = GetAllFormsUsecase(repo)
+controller = GetAllFormsController(usecase)
+
+
+def lambda_handler(event, context):
+    httpRequest = LambdaHttpRequest(data=event)
+    httpRequest.data['requester_user'] = event.get('requestContext', {}).get('authorizer', {}).get('claims', None)
+    response = controller(httpRequest)
+    httpResponse = LambdaHttpResponse(status_code=response.status_code, body=response.body, headers=response.headers)
+    return httpResponse.toDict()
