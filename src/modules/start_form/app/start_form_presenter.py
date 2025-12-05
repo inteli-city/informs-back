cebb748f1d1061bf6352d344d08e58075a3e94ab
Rodrigo Siqueira <rodrigo.dsiqueira1@gmail.com>
2025-12-05 11:58:16 -0300

    feat: adicionar decorador de manipulador de erros ao lambda_handler

--- src/modules/start_form/app/start_form_presenter.py
+++ src/modules/start_form/app/start_form_presenter.py
@@ -1,6 +1,7 @@
 from .start_form_controller import StartFormController
 from .start_form_usecase import StartFormUsecase
 from src.shared.environments import Environments
+from src.shared.helpers.error_handler import lambda_error_handler
 from src.shared.helpers.external_interfaces.http_lambda_requests import LambdaHttpRequest, LambdaHttpResponse
 
 
@@ -8,7 +9,7 @@ repo = Environments.get_form_repo()()
 usecase = StartFormUsecase(repo)
 controller = StartFormController(usecase)
 
-
+@lambda_error_handler
 def lambda_handler(event, context):
     httpRequest = LambdaHttpRequest(data=event)
     httpRequest.data['requester_user'] = event.get('requestContext', {}).get('authorizer', {}).get('claims', None)
