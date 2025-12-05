e2819dc85ae859975817d52d2dfa34f67f9ab6e6
Rodrigo Siqueira <rodrigo.dsiqueira1@gmail.com>
2025-12-05 14:15:20 -0300

    feat: adicionar controlador, caso de uso e apresentador para submissão de formulários

--- src/modules/submit_form/app/submit_form_presenter.py
+++ src/modules/submit_form/app/submit_form_presenter.py
@@ -1,14 +1,17 @@
-from .complete_form_controller import CompleteFormController
-from .complete_form_usecase import CompleteFormUsecase
+from .submit_form_controller import SubmitFormController
+from .submit_form_usecase import SubmitFormUsecase
 from src.shared.environments import Environments
+from src.shared.helpers.error_handler import lambda_error_handler
 from src.shared.helpers.external_interfaces.http_lambda_requests import LambdaHttpRequest, LambdaHttpResponse
 
 
 repo = Environments.get_form_repo()()
 image_repo = Environments.get_image_repo()()
-usecase = CompleteFormUsecase(repo, image_repo)
-controller = CompleteFormController(usecase)
+usecase = SubmitFormUsecase(repo, image_repo)
+controller = SubmitFormController(usecase)
 
+
+@lambda_error_handler
 def lambda_handler(event, context):
     
     httpRequest = LambdaHttpRequest(data=event)
