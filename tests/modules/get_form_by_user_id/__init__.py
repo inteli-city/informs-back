284a37d5c920c45b306048a8337ef86c9b74bdae
Rodrigo Siqueira <rodrigo.dsiqueira1@gmail.com>
2025-12-30 11:54:07 -0300

    Remove endpoints and tests for get_form_by_user_id and update_form_status modules

--- tests/modules/get_form_by_user_id/__init__.py
+++
@@ -1,3 +0,0 @@
-import pytest
-
-pytestmark = pytest.mark.skip(reason="Temporarily skipped: get_form_by_user_id not in current scope")
--- tests/modules/update_form_status/__init__.py
+++
@@ -1,3 +0,0 @@
-import pytest
-
-pytestmark = pytest.mark.skip(reason="Temporarily skipped: update_form_status not in current scope")
